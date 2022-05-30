b2371f538a0740583448c9d5ff5f48de3ec0a5a8
chuminhtan <chuminhtan.work@gmail.com>
2022-05-30 17:02:07 +0700

    Update deep-lin html page for testing

--- api/teams.js
+++ api/teams.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require('path');
 router.get('/teams', (req, res) => {
-    console.log(req.cookies);
-    res.send(req.cookies);
+    res.sendFile(path.join(__dirname + '/deep-link.html'));
 });
 
 router.post('/teams', (req, res) => {
